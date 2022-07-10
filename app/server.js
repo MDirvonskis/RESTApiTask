7ce637f0c9c8792639533a6c5e8dcc4c8f67f7e0
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-07-10 21:17:44 +0100

    Created list object

--- app/server.js
+++ app/server.js
@@ -2,18 +2,26 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+Attendees=[];
+
+
 app.get('/', (req, res) => {
+
   res.send('Hello World!')
 });
-app.post('/', (req, res) => {
-    res.send('Got a POST request')
+app.post('/List', (req, res) => {
+
+  res.send('Got a POST request')
   });
-app.put('/user', (req, res) => {
+app.put('/List/Insert', (req, res) => {
+
   res.send('Got a PUT request at /user')
 });
-app.delete('/user', (req, res) => {
-    res.send('Got a DELETE request at /user')
+app.delete('/List/Delete', (req, res) => {
+
+  res.send('Got a DELETE request at /user')
   });
 app.listen(port, () => {
+
   console.log(`Example app listening on port ${port}`)
 });
