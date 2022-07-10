595f34c287422e9e4a70a98ee8fbf3c882294c7e
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-07-10 23:36:29 +0100

    Created function to find id from Attendees Added status codes. Changed roots for app/.

--- app/server.js
+++ app/server.js
@@ -4,30 +4,58 @@ const port = 8000;//Port of the server
 
 Attendees=[
   {
-  id:"1",//Int Object identifier 
+  id:"1",//Int Object identifier
   name:"",//String
   notes:""//String
   }
 ];
-
-
+findId(id)
+{
+  Attendees.filter(function(element){
+    if(element.id == id)//Check if given id is contained in list.
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  });
+}
 app.get('/', (req, res) => {
-  //Send html?
-  res.send('Hello World!')
+  //Send html?  
+  res.send('Server started...').status(200);
 });
-app.post('/List', (req, res) => {
+app.get('/List', (req, res) => {
   //Send list objects
   res.send('Got a POST request')
-  });
-app.put('/List/Insert', (req, res) => {
-  //Add to list
-  res.send('Got a PUT request at /user')
+  res.send(Attendees).status(200);//success
 });
-app.delete('/List/Delete', (req, res) => {
-  //Delete from list
+app.post('/List/', (req, res) => {
+  //Check
+  //Object.keys(req.).toString()
+  if(findId(req.params.id))
+  {
+    res.send("Id already exist.").status(302)//302 Found
+    //errorlog
+  }
+  else{
+    Attendees.push(req.body)//Add new attendee to list (id, name, notes);
+  }  
+});
+app.delete('/List/:userID', (req, res) => {
   res.send('Got a DELETE request at /user')
+
+  if(findId(((req.body).id)).toString)
+  {
+    Attendees.delete(req.body);
+    //delete item from list
+  }
+  else{
+    res.send("Item not found").status(404);
+    //Log error
+  }
   });
 app.listen(port, () => {
-
   console.log(`Server port:${port}`)
 });
