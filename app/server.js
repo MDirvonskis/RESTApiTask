9573cc841080aa482994338ca9de6d3ab8eb8acb
Martynas Dirvonskis <md597@canterbury.ac.uk>
2022-07-10 21:26:40 +0100

    Added to list object.

--- app/server.js
+++ app/server.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = 8000;//Port of the server
 
-Attendees=[];
+Attendees=[
+  {
+  id:"1",//Int Object identifier 
+  name:"",//String
+  notes:""//String
+  }
+];
 
 
 app.get('/', (req, res) => {
-
+  //Send html?
   res.send('Hello World!')
 });
 app.post('/List', (req, res) => {
-
+  //Send list objects
   res.send('Got a POST request')
   });
 app.put('/List/Insert', (req, res) => {
-
+  //Add to list
   res.send('Got a PUT request at /user')
 });
 app.delete('/List/Delete', (req, res) => {
-
+  //Delete from list
   res.send('Got a DELETE request at /user')
   });
 app.listen(port, () => {
 
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server port:${port}`)
 });
